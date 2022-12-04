718a1df221c2faad2378a77ce3033b76840d1e01
Ty <dtylerlong@gmail.com>
2022-12-04 08:39:48 -0500

    cleaning up the code

--- Day 1/Solution.py
+++ Day 1/Solution.py
@@ -1,6 +1,6 @@
 from itertools import groupby
 
-inputFile = open("input.txt", 'r') #not full path
+inputFile = open("input.txt", 'r') # not full path
 
 data = inputFile.read()
 
@@ -12,13 +12,14 @@ for i in dataInList:
     try:
         e = int(i)
         result.append(e)
-    except:
-        result.append('')
+    except ValueError:
+        result.append('')  # Re-add the '' so we can break up into subgroup lists
 
 newResult = [list(sub) for element, sub in groupby(result, key=bool) if element]
-
+# Sum all subgroups
 sumList = [sum(l) for l in newResult]
 
+# Part 1 Top Amount
 print("Part 1: " + str(sum(sorted(sumList, reverse=True)[:1])))
-
+# Part 2 Top Three
 print("Part 2: " + str(sum(sorted(sumList, reverse=True)[:3])))
