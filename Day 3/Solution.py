107fe472f1ae046d01c3f58ac0f4ebb7f522a0de
Ty <dtylerlong@gmail.com>
2022-12-04 20:48:58 -0500

    Day 3 part 1

--- Day 3/Solution.py
+++ Day 3/Solution.py
@@ -1,12 +1,32 @@
-inputFile = "input.txt" #Not full path
+inputFile = "input.txt"  # Not full path
 
 result = []
 with open(inputFile, 'r') as fp:
     for i in fp.readlines():
-        tmp = i.split(' ')
+        tmp = i.split()
         try:
             result.append(str(tmp[0].replace("\n", "")))
         except ValueError:
             pass
 
-print(result)
+# print(result)
+count = 0
+
+
+def priority_count(item):
+    if item.islower():
+        return ord(item) - ord('a') + 1
+    else:
+        return ord(item) - ord('A') + 27
+
+
+for line in result:
+    ruck = list(line)
+    ruckSplit = int(len(ruck) / 2)
+    ruckCompartment = ruck[:ruckSplit], ruck[ruckSplit:]
+
+    for i in ruckCompartment[0]:
+        if i in ruckCompartment[1]:
+            count += priority_count(i)
+            break
+print("Count: " + str(count))
