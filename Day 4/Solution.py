6a4b51b6bdcd6890c7a30d99039d4c9cd19a8759
Ty <dtylerlong@gmail.com>
2022-12-06 08:54:27 -0500

    Day 4 Complete

--- Day 4/Solution.py
+++ Day 4/Solution.py
@@ -3,14 +3,21 @@ data = inputFile.read().split()
 inputFile.close()
 
 count = 0
+assignment = 0
 
 for x in data:
     pair = x.split(',')
     section1 = list(map(int, pair[0].split('-')))
     section2 = list(map(int, pair[1].split('-')))
 
+    # Part One
     if (section1[0] <= section2[0] and section1[1] >= section2[1]) or \
             (section2[0] <= section1[0] and section2[1] >= section1[1]):
         count += 1
 
+    # Part Two
+    if max(section1[0], section2[0]) <= min(section1[1], section2[1]):
+        assignment += 1
+
 print("Part 1: " + str(count))
+print("Part 2: " + str(assignment))
