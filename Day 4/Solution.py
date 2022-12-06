41c9808533a64ca506b3f5ca4e50556b1022d707
Ty <dtylerlong@gmail.com>
2022-12-05 22:05:30 -0500

    day 4 part 1 done

--- Day 4/Solution.py
+++ Day 4/Solution.py
@@ -6,9 +6,9 @@ dataInList = data.split()
 count = 0
 
 for x in dataInList:
-    a = x.split(',')
-    section1 = list(map(int, a[0].split('-')))
-    section2 = list(map(int, a[1].split('-')))
+    pair = x.split(',')
+    section1 = list(map(int, pair[0].split('-')))
+    section2 = list(map(int, pair[1].split('-')))
 
     if (section1[0] <= section2[0] and section1[1] >= section2[1]) or \
             (section2[0] <= section1[0] and section2[1] >= section1[1]):
