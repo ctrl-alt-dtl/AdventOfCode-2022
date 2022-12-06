d49efc8cfd099e4091d3112e90dc3693d0881085
Ty <dtylerlong@gmail.com>
2022-12-05 21:21:29 -0500

    day 4 - wip

--- Day 4/Solution.py
+++ Day 4/Solution.py
@@ -3,16 +3,25 @@ inputFile = "input2.txt"  # Not full path
 with open(inputFile, 'r') as fp:
     result = fp.read().splitlines()
 
-print(result)
+# print(result)
 
-out = []
-for i in result:
-    out.extend(list(i.split(',')))
+count = 0
 
-print(out)
+for x in range(len(result)-1):
+    a = result[x].split(',')
+    # print(a[0], a[1])
 
+    section1 = a[0].split('-')
+    section2 = a[1].split('-')
 
+    if section1[0] >= section2[0] and section1[1] <= section2[1] or \
+            section2[0] >= section1[0] and section2[1] <= section1[1]:
+        print(section1[0], section1[1])
+        print(section2[0], section2[1])
+        print("")
+        count += 1
 
+print(count)
 
 
 
