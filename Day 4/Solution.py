e6279270c53628f733d299f7aefa7b90e0c3f56c
Ty <dtylerlong@gmail.com>
2022-12-05 20:47:30 -0500

    day 4 - wip

--- Day 4/Solution.py
+++ Day 4/Solution.py
@@ -1,23 +1,15 @@
 inputFile = "input2.txt"  # Not full path
 
-result = []
 with open(inputFile, 'r') as fp:
-    for i in fp.readlines():
-        tmp = i.split()
-        try:
-            result.append(str(tmp[0]))
-        except ValueError:
-            pass
+    result = fp.read().splitlines()
 
 print(result)
 
-crossAssignment = 0
-pair = [[result[i]] for i in range(len(result)-1)]
-print(pair)
-for x in pair:
-    for i in x:
-        section = i.split(',')
-        print(section)
+out = []
+for i in result:
+    out.extend(list(i.split(',')))
+
+print(out)
 
 
 
