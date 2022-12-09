0fe953a357d31a7c6540b6811bc724ba672d6459
Ty <dtylerlong@gmail.com>
2022-12-08 21:10:43 -0500

    cleaning up day 5

--- Day 5/Solution.py
+++ Day 5/Solution.py
@@ -1,3 +1,5 @@
+import copy
+
 # Because I am lazy and do not want to write code to parse this multi-2d list
 cargo_crates = [
     ['H', 'C', 'R'],
@@ -15,13 +17,12 @@ test_crates = [
     ['M', 'C', 'D'],
     ['P']
 ]
-
-inputFile = "input.txt"  # Not full path
-result = []
+# Making a deep copy of the list for Part 2, because reasons...
+cargo_crates2 = copy.deepcopy(cargo_crates)
 
 # Part 1
 moves = []  # final list for moves
-with open(inputFile, 'r') as fp:
+with open("input.txt", 'r') as fp:
     for i in fp.readlines():
         tmp = i.split()
         all_moves = []  # parsing all moves in each odd index of each read line
@@ -32,10 +33,17 @@ with open(inputFile, 'r') as fp:
                 pass
         moves.append(all_moves)
 
+for num, start, finish in moves:
+    for i in range(num):
+        x = cargo_crates[start - 1].pop()
+        cargo_crates[finish - 1].append(x)
+
+print("Part 1: " + "".join([a[-1] for a in cargo_crates]))
+
 # Part 2 - This hurt my head.....
 for num, start, finish in moves:
-    multi_stack = cargo_crates[start - 1][-num:]
-    cargo_crates[start - 1] = cargo_crates[start - 1][:-num]
-    cargo_crates[finish - 1] += multi_stack
+    multi_stack = cargo_crates2[start - 1][-num:]
+    cargo_crates2[start - 1] = cargo_crates2[start - 1][:-num]
+    cargo_crates2[finish - 1] += multi_stack
 
-print("Part 2: " + "".join([a[-1] for a in cargo_crates]))
+print("Part 2: " + "".join([a[-1] for a in cargo_crates2]))
