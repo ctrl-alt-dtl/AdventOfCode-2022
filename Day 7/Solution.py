c4c9af11604ff5fa12b66fac834f8c8fadf2f05c
Ty <dtylerlong@gmail.com>
2022-12-19 19:37:17 -0500

    working on Day 7

---
+++ Day 7/Solution.py
@@ -0,0 +1,35 @@
+inputFile = open("input2.txt", 'r')
+data = inputFile.read().split("\n")
+inputFile.close()
+
+rep = []
+for x in data:
+    rep.append(x.split(" "))
+# print(list(rep))
+class Node:
+    def __init__(self, index, parent=None, size=0):
+        self.index = index
+        self.parent = parent
+        self.content = {}
+        self.size = size
+        self.lessSize = size
+
+root = Node(0)
+index = 1
+nodeDictonary = {}
+current = root
+
+# Walk the directory
+for line in rep:
+    if line[0] == "$":
+        if line[1] == "cd":
+            if line[2] == "..":
+                current = root
+            else:
+                directoryName = line[2]
+                if directoryName == "/":
+                    current = root
+                else:
+                    current = current.content[directoryName]
+
+
