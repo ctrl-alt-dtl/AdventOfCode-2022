42bd2d2e3f9878ef60b853ace20c94c4e428a0b1
Ty <dtylerlong@gmail.com>
2022-12-20 22:41:32 -0500

    working on Day 7

--- Day 7/Solution.py
+++ Day 7/Solution.py
@@ -7,29 +7,33 @@ data = []
 for x in rawData:
     data.append(x.split(" "))
 
-directoryDict = root = {}
-
-paths = []
+directoryDict = {}
 
 for line in data:
+    # print(line)
     if line[0] == '$':
         if line[1] == 'cd':
-            if line[0] == "dir":
-                if "dir" not in directoryDict:
-                    directoryDict["dir"] = [line[1]]
+            if line[2] == '/':
+                if "root" not in directoryDict:
+                    directoryDict["root"] = [line[2]]
                 else:
                     directoryDict["dir"].append(line[1])
 
+    if line[0] == "dir":
+        if "dir" not in directoryDict:
+            directoryDict["dir"] = [line[1]]
+        else:
+            directoryDict["dir"].append(line[1])
+
     if line[0] != "$" and line[0] != "dir":
         # print(line)
-        if "fileSize" not in directoryDict:
-            directoryDict["fileSize"] = ([line[0]])
+        if "size" not in directoryDict:
+            directoryDict["size"] = ([line[0]])
         else:
-            directoryDict["fileSize"].append((line[0]))
-        if "fileName" not in directoryDict:
-            directoryDict["fileName"] = [line[1]]
-        directoryDict["fileName"].append(line[1])
-    print(line)
+            directoryDict["size"].append((line[0]))
+        if "name" not in directoryDict:
+            directoryDict["name"] = [line[1]]
+        directoryDict["name"].append(line[1])
 
 print(directoryDict)
 
