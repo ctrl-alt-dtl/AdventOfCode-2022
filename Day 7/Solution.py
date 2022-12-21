9f83d41ad46d6761c71c0dee10573c67ba208ac1
Ty <dtylerlong@gmail.com>
2022-12-20 19:50:28 -0500

    reworking day 7

--- Day 7/Solution.py
+++ Day 7/Solution.py
@@ -1,4 +1,4 @@
-import pathlib
+from collections import defaultdict
 inputFile = open("input2.txt", 'r')
 rawData = inputFile.read().split("\n")
 inputFile.close()
@@ -7,23 +7,29 @@ data = []
 for x in rawData:
     data.append(x.split(" "))
 
-directoryDict = {}
+directoryDict = root = {}
+
+paths = []
 
 for line in data:
-    #print(line)
-    if line[0] == "dir":
-        if "dir" not in directoryDict:
-            directoryDict["dir"] = []
-        directoryDict["dir"].append(line[1])
+    if line[0] == '$':
+        if line[1] == 'cd':
+            if line[0] == "dir":
+                if "dir" not in directoryDict:
+                    directoryDict["dir"] = [line[1]]
+                else:
+                    directoryDict["dir"].append(line[1])
 
-    if line[0] != '$' and line[0] != "dir":
-        #print(line)
+    if line[0] != "$" and line[0] != "dir":
+        # print(line)
         if "fileSize" not in directoryDict:
-            directoryDict["fileSize"] = []
-        directoryDict["fileSize"].append(int(line[0]))
+            directoryDict["fileSize"] = ([line[0]])
+        else:
+            directoryDict["fileSize"].append((line[0]))
         if "fileName" not in directoryDict:
-            directoryDict["fileName"] = []
+            directoryDict["fileName"] = [line[1]]
         directoryDict["fileName"].append(line[1])
-
+    print(line)
 
 print(directoryDict)
+
